cb7c3b489269354fa278d0039d8a9d9c3e361b2f
luka-casey <luka.casey@gmail.com>
2025-06-18 17:17:16 +1000

    WIP adding stuff

--- backend-api/CodeReviewApi/Controllers/test.cs
+++ backend-api/CodeReviewApi/Controllers/test.cs
@@ -2,25 +2,14 @@ namespace CodeReviewApi.Controllers;
 
 public class test
 {
-    
-    public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public DateTime DateOfBirth { get; set; }
-    public string Email { get; set; }
-    public bool IsActive { get; set; }
-     
      public int GetAge()
      {
-         var today = DateTime.Today;
-         var age = today.Year - DateOfBirth.Year;
-         if (DateOfBirth.Date > today.AddYears(-age)) age--;
-         return age;
+         return 25;
      }
     
     public override string ToString()
     {
-        return $"{FirstName} {LastName} ({Email})";
+        return "Ground hog day";
     }
     
 }
