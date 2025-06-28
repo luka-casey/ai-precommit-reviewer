1e6d7590f537891ad451632fdf5c76eb8a15b5f5
luka-casey <luka.casey@gmail.com>
2025-06-28 15:06:19 +1000

    dummy page for screenshot

---
+++ git-reviewer/src/button.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Button: React.FC = () => {
+  return (
+    <button className="p-2 bg-green-500 text-white rounded">
+      Submit
+    </button>
+  );
+};
