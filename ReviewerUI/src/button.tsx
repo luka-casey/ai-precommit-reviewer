5a200db4d3acfd3563dc6b2ba4c56ea7c8b764a8
luka-casey <luka.casey@gmail.com>
2025-06-29 17:11:33 +1000

    added sln for api

--- ReviewerUI/src/button.tsx
+++ ReviewerUI/src/button.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-// this is a test
+// this is a tests
 export const Button: React.FC = () => {
   return (
     <button className="p-2 bg-green-500 text-white rounded">
